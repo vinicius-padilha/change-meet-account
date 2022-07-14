392749de701a3ebb20c7007959c2aeeb752549e8
Vinícius Padilha <vinicius.padilha@outlook.com.br>
2022-07-14 18:24:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const isGoogleMeet = location.origin.includes('meet');
 const isDefaultAccount = location.search.includes('0') || !location.search.includes('authuser');
 
 if (isGoogleMeet && isDefaultAccount) {
-  const accountIndex = prompt('Alterar conta? Digite o index referente a ela.')
+  const accountIndex = prompt('Alterar conta padrão? Digite o index referente a conta escolhida.')
 
   if (accountIndex) {
     window.location = window.location.origin + window.location.pathname + `?authuser=${accountIndex}`;
